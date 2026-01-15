0599e1c7c453bcf531cc67999b7f7a3efaabbba6
BoomeshKS <168462111+BoomeshKS@users.noreply.github.com>
2026-01-15 14:50:38 +0530

    Update ALLOWED_HOSTS for production settings

--- server/settings.py
+++ server/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-_w(y7j@44+nd*sp$75ez9v-sru^@n-z*d!b1_)nc+tcg@m*d-x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['13.51.197.217','*']
 
 
 # Application definition
